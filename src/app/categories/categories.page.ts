8a60a8dc141a231f8411784cad5e8e99c3125314
Dudix93 <dodo45693@gmail.com>
2020-01-23 00:56:02 +0100

    Delete boxes & edit boxes names

--- src/app/categories/categories.page.ts
+++ src/app/categories/categories.page.ts
@@ -4,6 +4,7 @@ import { Storage } from '@ionic/storage';
 import { Box } from '../../models/Box';
 import { ItemsPage } from '../items/items.page';
 import { Router, NavigationExtras } from '@angular/router';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-categories',
@@ -66,6 +67,85 @@ export class CategoriesPage implements OnInit {
         ]
       });
       await alert.present();
+  }
+
+  async editTheBox(passedBox) {
+    const alert = await this.alertCtrl.create({
+      header: 'Edit Box',
+      inputs: [
+        {
+          name: 'newBoxName',
+          type: 'text',
+          value: passedBox.name
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+          }
+        }, {
+          text: 'Ok',
+          handler: data => {
+            let newName = data['newBoxName'];
+            if (newName) {
+              this.storage.get('boxes').then((boxes: Array<Box>) => {
+                boxes.forEach((box: Box, index) => {
+                  if (box.name === passedBox.name) {
+                    box.name = newName;
+                    this.storage.set('boxes', boxes);
+                    this.boxes = boxes;
+                    return false;
+                  }
+                });
+              });
+              this.showToast("Box name changed.");
+            } 
+            else {
+              this.showToast("Box name can't be empty!.");
+            }
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async deleteTheBox(passedBox) {
+    if (passedBox.items.length === 0) {
+      const alert = await this.alertCtrl.create({
+        header: 'Would you like to delete this box?',
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: () => {
+            }
+          }, 
+          {
+            text: 'Ok',
+            handler: () => {
+              this.storage.get('boxes').then((boxes: Array<Box>) => {
+                boxes.forEach((box: Box, index) => {
+                  if (box.name === passedBox.name) {
+                    boxes.splice(index, 1);
+                    this.storage.set('boxes', boxes);
+                    this.boxes = boxes;
+                    this.showToast("Box "+passedBox.name+" deleted.");
+                    return false;
+                  }
+                });
+              });
+            }
+          }
+        ]
+      });
+      await alert.present();
+    }
+    else this.showToast("Box"+passedBox.name+" can't be deleted."); 
   }
 
   showTheBox(box: Box) {
